b5921b731efc5173443cc3c4968d3fe3cf229e5a
murat <svsmurat358@gmail.com>
2023-04-27 22:29:46 +0200

    Initial commit

---
+++ src/test/java/techproed/tests/day22_Annotation/C02_BeforeAfterMethod.java
@@ -0,0 +1,23 @@
+package techproed.tests.day22_Annotation;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class C02_BeforeAfterMethod {
+
+    @Test
+    public void techproedTest() {
+
+    }
+
+    @Test
+    public void amozon() {
+
+    }
+
+    @BeforeMethod
+    public void setUp() {
+
+    }
+}
+
